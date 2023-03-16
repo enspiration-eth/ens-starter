0b1ffc1127717eb7fba0ba158eaee4bc10693a78
Enspiration Eth <enspiration.eth@gmail.com>
2023-03-16 18:16:40 +0200

    use pages folder

---
+++ src/components/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen pt-8 pb-16 flex flex-col gap-10 md:gap-20 items-center justify-center">
+      page
+    </main>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Page from "~/components/page";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>title</title>
+        <meta name="description" content="title" />
+      </Head>
+      <Page />
+    </>
+  );
+}
